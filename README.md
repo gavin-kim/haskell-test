f4664ade975ec7730cd9e1412e748087f0f0085b
Yoonkwan <kyk0704@hotmail.com>
2017-03-30 00:19:08 -0400

    .

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Haskell
 
 ### Standard Typeclasses
+
+    A typeclass is a sort of interface that defines some behavior.
+    A type supports and implements the behavior the typeclass describes.
+    Int(type) is a part of Integral(typeclass).
+     
 ![StandardTypeclasses](/images/StandardTypeclasses.png)
 
 ### Hierarchy of Numeric Typeclasses
--- src/ConvertingNumbers.hs
+++ src/ConvertingNumbers.hs
@@ -1,3 +1,13 @@
 -- NOTE: Conversion between numerical types in Haskell must be done explicitly
 
--- Integral 
+-- 1. Converting from and between Integral types
+-- NOTE: Integral types contain non-fractional numbers
+
+-- fromIntegral :: (Num b, Integral a) => a -> b
+-- fromInteger :: Num a => Integer-> a
+-- toInteger :: (Integral a) => a -> Integer
+
+
+-- 2. Converting from and between real-fractional types (rational-like types)
+-- NOTE: RealFrac types can contain either whole numbers or fractions.
+--       Rational
